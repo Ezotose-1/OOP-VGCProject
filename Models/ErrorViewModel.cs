347920af0bbd5d450a941d8976735e91d9f2bd5b
Pierre <60015586+Ezotose-1@users.noreply.github.com>
2021-04-05 14:24:10 +0100

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OOP_VGCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
