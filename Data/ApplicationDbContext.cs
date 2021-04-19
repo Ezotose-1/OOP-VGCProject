8caa9e8fbc0993026adbc210ba7b96f84167725b
julien calisto <julien.calisto@epita.fr>
2021-04-19 18:33:34 +0100

    Grades model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,7 +16,7 @@ namespace OOP_VGCProject.Data
         {
         }
         public DbSet<OOP_VGCProject.Models.Fees> Fees { get; set; }
-
+        public DbSet<OOP_VGCProject.Models.Grades> Grades { get; set; }
         public DbSet<OOP_VGCProject.Models.UserFee> UserFee { get; set; }
 
     }
