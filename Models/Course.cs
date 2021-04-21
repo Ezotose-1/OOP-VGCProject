b0bda94d103dfcbaf805437d87b3ba81f3a6c636
Loïc <loic.schussler@epita.fr>
2021-04-21 18:29:28 +0200

    [Add] TimeTable connection to DB

---
+++ Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOP_VGCProject.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+        public int FacultyId { get; set; }
+        public DateTime StartingTime { get; set; }
+        public DateTime EndingTime { get; set; }
+        public int GroupId { get; set; }
+    }
+}
