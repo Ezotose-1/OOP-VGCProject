c5caddb62b0d1670b652bbad0db2944b9c0a14f3
Pierre <pierre.blaess@gmail.com>
2021-04-21 16:22:03 +0100

    [ADD] Exams system

---
+++ Models/Exams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOP_VGCProject.Models
+{
+    public class Exams
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string ExamName { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public int CourseId { get; set; }
+    }
+}
